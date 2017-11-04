d440d26b7b6b1b6eecdffb2ec6e49f8c5574ee71
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2017-11-04 23:18:59 +0000

    Add facebook auth

--- app/routes.js
+++ app/routes.js
@@ -33,6 +33,22 @@ module.exports = function (app, passport) {
   })
   
   
+  // FACEBOOK
+  
+  // Redirect the user to Facebook for authentication.  When complete,
+  // Facebook will redirect the user back to the application at
+  //     /auth/facebook/callback
+  app.get('/auth/facebook', passport.authenticate('facebook', {scope: ['email']}));
+  
+  // Facebook will redirect the user to this URL after approval.  Finish the
+  // authentication process by attempting to obtain an access token.  If
+  // access was granted, the user will be logged in.  Otherwise,
+  // authentication has failed.
+  app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', { successRedirect: '/profile',
+                                      failureRedirect: '/' })
+  );
+  
   // Logged in
   app.get('/profile', isLoggedIn, (req, res) => {
     res.render('profile.ejs', {user: req.user})
